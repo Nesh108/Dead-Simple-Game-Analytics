7b9feb6f8bb4494e4502d9c9c61f5f1ab40986df
Alberto Vaccari <alberto.vaccari@mequilibrium.com>
2022-07-03 19:16:55 +0200

    fixed ordering for export

--- pkg/controllers/ExportEvents.go
+++ pkg/controllers/ExportEvents.go
@@ -2,11 +2,11 @@ package controllers
 
 import (
 	"encoding/csv"
+	"fmt"
 	"net/http"
 	"os"
-	"strconv"
 	"os/exec"
-	"fmt"
+	"strconv"
 
 	"github.com/Nesh108/Dead-Simple-Game-Analytics/pkg/models"
 )
@@ -16,31 +16,32 @@ func (c controller) ExportEvents(w http.ResponseWriter, r *http.Request) {
 
 	exportName := "complete"
 	projectParam, ok := r.URL.Query()["project"]
-    if !ok || len(projectParam[0]) < 1 {
+	if !ok || len(projectParam[0]) < 1 {
 		if result := c.DB.Find(&events); result.Error != nil {
 			c.UnhandledErrorResponse(w, result.Error)
 			return
 		}
-    } else {
+	} else {
 		project := projectParam[0]
 		exportName = project
-		if result := c.DB.Where("project_name = ?", project).Find(&events); result.Error != nil {
+		if result := c.DB.Where("project_name = ?", project).Order("id asc").Find(&events); result.Error != nil {
 			c.UnhandledErrorResponse(w, result.Error)
 			return
 		}
 	}
 
 	filename := "exports/" + exportName + "_export.csv"
-    f, err := os.Create(filename)
-    defer f.Close()
+	f, err := os.Create(filename)
 
-    if err != nil {
+	if err != nil {
 		c.UnhandledErrorResponse(w, err)
 		return
-    }
+	}
 
-    writer := csv.NewWriter(f)
-	writer.Write([]string{"id","user_id","project_name","event_key","event_value","request_id","timestamp"})
+	defer f.Close()
+
+	writer := csv.NewWriter(f)
+	writer.Write([]string{"id", "user_id", "project_name", "event_key", "event_value", "request_id", "timestamp"})
 
 	for _, value := range events {
 		err = writer.Write([]string{
@@ -51,21 +52,20 @@ func (c controller) ExportEvents(w http.ResponseWriter, r *http.Request) {
 			value.EventValue,
 			value.RequestId,
 			value.Timestamp.Format("2006-01-02 15:04:05")})
-        
+
 		if err != nil {
 			writer.Flush()
 			c.UnhandledErrorResponse(w, err)
 			return
 		}
-    }
+	}
 	writer.Flush()
 	output, errOutput := exec.Command(os.Getenv("EXPORT_COMMAND_PATH")).Output()
 	fmt.Printf("OUT: %s\n", output)
-    if errOutput != nil {
-        c.UnhandledErrorResponse(w, err)
+	if errOutput != nil {
+		c.UnhandledErrorResponse(w, err)
 		return
-    }
+	}
 
 	c.SuccessResponse(w)
-	return
 }
--- pkg/controllers/controllers.go
+++ pkg/controllers/controllers.go
@@ -1,44 +1,41 @@
 package controllers
 
 import (
-    "gorm.io/gorm"
-    "encoding/json"
-    "net/http"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"gorm.io/gorm"
 
 	"github.com/Nesh108/Dead-Simple-Game-Analytics/pkg/models"
 )
 
 type controller struct {
-    DB *gorm.DB
+	DB *gorm.DB
 }
 
 func New(db *gorm.DB) controller {
-    return controller{db}
+	return controller{db}
 }
 
 // use http statuses for different errors: https://go.dev/src/net/http/status.go
 func (c controller) ValidationErrorResponse(w http.ResponseWriter, message string) {
-    c.Response(w, message, http.StatusBadRequest)
-    return
+	c.Response(w, message, http.StatusBadRequest)
 }
 
 func (c controller) UnhandledErrorResponse(w http.ResponseWriter, err error) {
-    fmt.Println(err)
-    c.Response(w, err.Error(), http.StatusInternalServerError)
-    return
+	fmt.Println(err)
+	c.Response(w, err.Error(), http.StatusInternalServerError)
 }
 
 func (c controller) SuccessResponse(w http.ResponseWriter) {
-    c.Response(w, "success", http.StatusOK)
-    return
+	c.Response(w, "success", http.StatusOK)
 }
 
 func (c controller) Response(w http.ResponseWriter, message string, statusCode int) {
 	w.Header().Add("Content-Type", "application/json")
-    w.WriteHeader(statusCode)
-    var response models.Response
-    response.Message = message
-    json.NewEncoder(w).Encode(response)
-    return
+	w.WriteHeader(statusCode)
+	var response models.Response
+	response.Message = message
+	json.NewEncoder(w).Encode(response)
 }
