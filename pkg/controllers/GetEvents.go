6911237e0bfe70b0891a32ce21c8e5e645df5cfd
lily <lily.bickerstaffe@gmail.com>
2023-02-15 11:47:49 +0100

    Add filtering events by last_id and since

--- pkg/controllers/GetEvents.go
+++ pkg/controllers/GetEvents.go
@@ -3,26 +3,39 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
-
+	"time"
 	"github.com/Nesh108/Dead-Simple-Game-Analytics/pkg/models"
 )
 
 func (c controller) GetEvents(w http.ResponseWriter, r *http.Request) {
 	var events []models.Event
+	query := c.DB
 
 	projectParam, ok := r.URL.Query()["project"]
-	if !ok || len(projectParam[0]) < 1 {
-		if result := c.DB.Order("id asc").Find(&events); result.Error != nil {
-			c.UnhandledErrorResponse(w, result.Error)
-			return
-		}
-	} else {
+	if ok && len(projectParam[0]) > 1 {
 		project := projectParam[0]
-		if result := c.DB.Where("project_name = ?", project).Order("id asc").Find(&events); result.Error != nil {
-			c.UnhandledErrorResponse(w, result.Error)
+		query = query.Where("project_name = ?", project)
+	}
+
+	sinceParam, ok := r.URL.Query()["since"]
+	if ok && len(sinceParam[0]) > 0 {
+		since, err := time.Parse("02-01-2006", sinceParam[0]) // expected date format is DD-MM-YYYY
+		if err != nil {
+			c.UnhandledErrorResponse(w, err)
 			return
 		}
+		query = query.Where("timestamp >= ?", since)
+	}
 
+	lastIdParam, ok := r.URL.Query()["last_id"]
+	if ok && len(lastIdParam[0]) > 0 {
+		lastId := lastIdParam[0]
+		query = query.Where("id > ?", lastId)
+	}
+
+	if result := query.Order("id asc").Find(&events); result.Error != nil {
+		c.UnhandledErrorResponse(w, result.Error)
+		return
 	}
 
 	w.Header().Add("Content-Type", "application/json")
